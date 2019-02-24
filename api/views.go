65175ae102135e658b9b16b29bbfb78f04372306
Henrique Lopes <riquellopes@gmail.com>
2019-02-23 23:45:28 -0300

    creating api

--- api/views.go
+++ api/views.go
@@ -1,1 +1,16 @@
 package api
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/riquellopes/store/camunda"
+)
+
+// Buy -
+func Buy(client *camunda.Client) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		client.Send("store")
+		return c.String(http.StatusOK, "Send to zeebe")
+	}
+}
